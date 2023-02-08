0b9c7902a470c9b9c87a1b52a2bef9b1739f6c91
Khamov Oleg <njspok@gmail.com>
2023-02-08 12:55:54 +0500

    initial

--- find_operation_formjla.pl
+++ find_operation_formjla.pl
@@ -1,4 +1,4 @@
-
+%  ((((1?2)?3)?4)?5)?6=35
 formula(X1, X2, X3, X4, X5, X6, Sign1, Sign2, Sign3, Sign4, Sign5, Result):-
 operation(X1, X2, Sign1, PartialResult1),
 operation(PartialResult1, X3, Sign2, PartialResult2),
