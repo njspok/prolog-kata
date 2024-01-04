67e94b39580fa67f95b60da4d3272c53f21609bf
Khamov Oleg <njspok@gmail.com>
2024-01-04 13:59:10 +0500

    simple electric scheme

---
+++ game_char_move.pl
@@ -0,0 +1,10 @@
+% реализация языка перемещний персонажа по плосоксти координат на GCD
+% ?- move(X, Y, [up, up, up, left, left, down], []).
+
+move(X, Y) --> step(X, Y).
+move(X, Y) --> step(X1, Y1), move(X2, Y2), {X is X1 + X2, Y is Y1 + Y2}.
+
+step(0, 1) --> [up].
+step(0, -1) --> [down].
+step(-1, 0) --> [left].
+step(1, 0) --> [right].
